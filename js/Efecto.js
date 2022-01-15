3d49e47ec5c506ee57d25bc5cd0e3b7e74cff031
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2022-01-14 22:16:32 -0500

    Portada Terminada

---
+++ js/Efecto.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+       /*alert('ola mundo')*/
+
+
+if (window.matchMedia('(min-width: 1910px)').matches)
+{
+     /*  alert('ola mundo')*/
+       
+     /*$(".svg_2").css({"transform": "scale(1.40, 1.4)","border": "5px solid blue",'width':"","right":"100%","height":"300px"});*/
+
+    // do functionality on screens smaller than 768px
+}
+});
