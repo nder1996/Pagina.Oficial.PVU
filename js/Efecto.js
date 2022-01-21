dcee09dda811ff0122ed1f0ad282518081b35398
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2022-01-20 21:23:29 -0500

    Nuevos Ajustes Version Beta

--- js/Efecto.js
+++ js/Efecto.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
 
   $('.nav-link').click(function() {
-     var active = $(this).attr("id");
-     if(active=='pills-contacts-tab'){
-        $('.title_modal_contact').html('<h5 class="title_modal_contact modal-title" id="exampleModalLabel">Contact Us</h5>')
-     }else{
-       $('.title_modal_contact').html('<h5 class="title_modal_contact modal-title" id="exampleModalLabel">Collaborations</h5>')
-     }
-  })
+    var active = $(this).attr("id");
+    if (active == 'pills-contacts-tab') {
+      $('.title_modal_contact').html('<h5 class="title_modal_contact modal-title" id="exampleModalLabel">Contact Us</h5>')
+    } else {
+      $('.title_modal_contact').html('<h5 class="title_modal_contact modal-title" id="exampleModalLabel">Collaborations</h5>')
+    }
+  });
+
+
 
 });
