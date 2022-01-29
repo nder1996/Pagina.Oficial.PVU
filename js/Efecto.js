51780051d850072d13f4d509ae26d7ec4de969da
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2022-01-29 00:03:35 -0500

    incompleto multilenguaje

--- js/Efecto.js
+++ js/Efecto.js
@@ -1,75 +1,77 @@
 $(document).ready(function() {
 
 
-  $('.btn_modal_details').click(function(){
-    alert('ola mundo')
-  })
-
-
-  $('.nav-link').click(function() {
+  /*$('.nav-link').click(function() {
     var active = $(this).attr("id");
     if (active == 'pills-contacts-tab') {
-      $('.title_modal_contact').html('<h5 class="title_modal_contact modal-title" id="exampleModalLabel">Contact Us</h5>')
+      $('.modal_title_contacts').html(`<h5 class="title_modal_contact modal-title lang" key='${contact}'  id="exampleModalLabel"></h5>`)
     } else {
-      $('.title_modal_contact').html('<h5 class="title_modal_contact modal-title" id="exampleModalLabel">Collaborations</h5>')
+      $('.modal_title_contacts').html(`<h5 class="title_modal_contact modal-title"  key='${modal_title_2}' id="exampleModalLabel"></h5>`)
     }
-  });
+  });*/
 
-  var ruleta = [
-    'Web development', 'Mobile development', 'UX/UI design', 'Software development', 'Responsive web design'
-  ]
 
-  let random_vector = []
-  let j = 0;
 
-  function Aleatorio() {
 
-    for (let i = 1; i <= 20; i++) {
-      let num_aletorio = 0 + Math.floor(Math.random() * 5)
-      if (random_vector.length == 0) {
-        random_vector.push(num_aletorio)
-      } else {
-        if (random_vector.indexOf(num_aletorio) == -1) {
-          if (random_vector.length <= 5) {
-            random_vector.push(num_aletorio)
-          }
+    /*-----------------------------FUNCION RULETA------------------------------*/
+
+
+
+    var ruleta = [
+        'Web development', 'Mobile development', 'UX/UI design', 'Software development', 'Responsive web design'
+    ]
+
+    let random_vector = []
+    let j = 0;
+
+    function Aleatorio() {
+
+        for (let i = 1; i <= 20; i++) {
+            let num_aletorio = 0 + Math.floor(Math.random() * 5)
+            if (random_vector.length == 0) {
+                random_vector.push(num_aletorio)
+            } else {
+                if (random_vector.indexOf(num_aletorio) == -1) {
+                    if (random_vector.length <= 5) {
+                        random_vector.push(num_aletorio)
+                    }
+                }
+            }
         }
-      }
+        return random_vector;
     }
-    return random_vector;
-  }
 
-  function Ruleta() {
-    for (let i = 1; i <= 20; i++) {
-      let num_aletorio = 0 + Math.floor(Math.random() * 5)
-      if (random_vector.length == 0) {
-        random_vector.push(num_aletorio)
-      } else {
-        if (random_vector.indexOf(num_aletorio) == -1) {
-          if (random_vector.length <= 5) {
-            random_vector.push(num_aletorio)
-          }
+    function Ruleta() {
+        for (let i = 1; i <= 20; i++) {
+            let num_aletorio = 0 + Math.floor(Math.random() * 5)
+            if (random_vector.length == 0) {
+                random_vector.push(num_aletorio)
+            } else {
+                if (random_vector.indexOf(num_aletorio) == -1) {
+                    if (random_vector.length <= 5) {
+                        random_vector.push(num_aletorio)
+                    }
+                }
+            }
         }
-      }
+        let contador = 0;
+        for (let i = 1; i <= 5; i++) {
+            console.log('contador : ', contador = contador + 1)
+            setTimeout(function() {
+                $(".Ruleta").append(`<p class='text_ruleta' id='ruleta_${i}'>${ruleta[random_vector[i-1]]}</p>`)
+            }, `${i}000`);
+        }
+        setTimeout(function() {
+            $(".text_ruleta").remove()
+        }, `6000`);
+        setTimeout(function() {
+            random_vector = []
+        }, `7000`);
     }
-    let contador = 0;
-    for (let i = 1; i <= 5; i++) {
-      console.log('contador : ', contador = contador + 1)
-      setTimeout(function() {
-        $(".Ruleta").append(`<p class='text_ruleta' id='ruleta_${i}'>${ruleta[random_vector[i-1]]}</p>`)
-      }, `${i}000`);
-    }
-    setTimeout(function() {
-      $(".text_ruleta").remove()
-    }, `6000`);
-    setTimeout(function() {
-      random_vector = []
-    }, `7000`);
-  }
 
-  setInterval(Ruleta, 8000);
+    setInterval(Ruleta, 8000);
 
 
-
+    /*-----------------------------FUNCION RULETA------------------------------*/
 
 });
